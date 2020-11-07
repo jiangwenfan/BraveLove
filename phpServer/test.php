b3fb7b5987416de54a2eb0447466ba3c5a6fbe58
miBook <zhan2103208467@gmail.com>
2020-11-07 21:18:20 +0800

    2020-11-7

---
+++ phpServer/test.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<h1>hello test!</h1>";
+
+?>
+<h2>234</h2>
+<form action="index.php" method="post">
+    <input type="text" name="name"/>
+    <input type="password" name="password"/>
+    <input type="submit"/>
+</form>
+<h1>
+    <h2></h2>
+</h1>
